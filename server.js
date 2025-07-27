5407c1eccff86e3de93ac70386b798f62ab86101
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-07-27 09:46:48 +0530

    feat : create simple express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require("express");
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Server is running...")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000...")
+})
+
