20695d98ba70294aa7bd502b289976fda2e2a441
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-08-17 13:01:31 +0530

    feat : ci cd check

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/",(req,res)=>{
     res.status(200).send("Server is running...")
 })
 
+app.get("/check",(req,res)=>{
+    res.status(200).send("CI CD is running...")
+})
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000...")
 })
